ccc2d9704fe884c12ea5d8021a2a6563bac1bdfa
Sebastien Nicoletti <sebastien.nicoletti@53js.fr>
2016-10-21 14:23:26 +0200

    Person class

---
+++ app/src/main/java/app/mbds/fr/unice/appbipper/entity/Person.java
@@ -0,0 +1,72 @@
+package app.mbds.fr.unice.appbipper.entity;
+
+/**
+ * Created by 53js-Seb on 21/10/2016.
+ */
+
+public class Person {
+
+    private String lastname;
+    private String firstname;
+    private String sex;
+    private String phone;
+    private String mail;
+    private String createdBy;
+    private String password;
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
